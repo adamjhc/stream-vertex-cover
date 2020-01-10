16a58a6852d22918e541a7b129dd5475571c4476
Adam Cox <ajc785@student.bham.ac.uk>
2020-01-10 18:30:15 +0000

    Added a method to brute force all vertex covers

--- src/vertex_cover/vertex_cover_brute_force.py
+++ src/vertex_cover/vertex_cover_brute_force.py
@@ -1,16 +1,11 @@
-"""
-"Once the kernel has been constructed, the vertex cover problem may be solved 
-by a brute force search algorithm that tests whether each subset of the kernel 
-is a cover of the kernel."
-"""
-
 from .vertex_cover_util import powerset, is_vertex_cover
 
 
 def vertex_cover_brute_force(graph: Graph):
     """
-    Tests all subsets of a given graph's nodes to check if any are a vertex 
-    cover of the given graph
+    Brute forces the subsets of the given graph to find a vertex cover
+
+    Returns the first found vertex cover
 
     Args:
     - graph (Graph): The graph to find a vertex cover of
@@ -22,3 +17,23 @@ def vertex_cover_brute_force(graph: Graph):
     for vertex_cover in ps_vertices:
         if is_vertex_cover(graph, vertex_cover):
             return vertex_cover
+
+    assert False, "This graph doesn't have a vertex cover?"
+
+
+def vertex_cover_brute_force_all(graph: Graph):
+    """
+    Brute forces the subsets of the given graph to find a vertex cover
+
+    Returns all vertex covers found as a generator
+
+    Args:
+    - graph (Graph): The graph to find a vertex cover of
+
+    Returns:
+    - list
+    """
+    ps_vertices = powerset(list(graph.nodes))
+    for vertex_cover in ps_vertices:
+        if is_vertex_cover(graph, vertex_cover):
+            yield vertex_cover
