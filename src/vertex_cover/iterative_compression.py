deb076bf923a15f020e539b9305052e948e83b4b
Adam Cox <ajc785@student.bham.ac.uk>
2020-01-13 16:48:58 +0000

    Function boilerplate

--- src/vertex_cover/iterative_compression.py
+++ src/vertex_cover/iterative_compression.py
@@ -1,2 +1,22 @@
 """
 """
+from networkx import Graph
+
+
+def vertex_cover_iterative_compression(graph: Graph, k: int) -> set:
+    """
+    Finds a vertex cover of at most size k using iterative compression
+
+    Parameters
+    ----------
+        graph : Graph
+            The graph to find a vertex cover of
+        k : int
+            Size k
+
+    Returns
+    -------
+        set
+            Vertex cover if one exists otherwise None
+    """
+    pass
