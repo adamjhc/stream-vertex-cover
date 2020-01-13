db381c45a9ebad01106a720b2d75827eb27f4049
Adam Cox <ajc785@student.bham.ac.uk>
2020-01-13 15:24:44 +0000

    Added a func to generate binary strings
    Plus a class to handle the branching

--- src/vertex_cover/binary_search_tree.py
+++ src/vertex_cover/binary_search_tree.py
@@ -3,6 +3,7 @@
 1-Pass BST usingO(k^2·logn)bits
 """
 from networkx import Graph
+from math import log2, ceil
 
 
 def vertex_cover_bst_2k_pass(graph: Graph, k: int) -> set:
@@ -41,5 +42,30 @@ def vertex_cover_bst_1_pass(graph: Graph, k: int) -> set:
     Returns
     -------
         set
+            Vertex cover if one exists otherwise None
     """
     pass
+
+
+def _get_binary_strings(k: int) -> list:
+    """
+    Calculates all binary strings up to a given value of k
+
+    Parameters
+    ----------
+        k : int
+            Value to generate binary strings up to
+
+    Returns
+    -------
+        list
+            List of binary strings
+    """
+    return [bin(i)[2:].rjust(ceil(log2(k)), "0") for i in range(k)]
+
+
+class _Node(object):
+    def __init__(self, value, left=None, right=None):
+        self.value = value  # The node value
+        self.left = left  # Left child
+        self.right = right  # Right child
