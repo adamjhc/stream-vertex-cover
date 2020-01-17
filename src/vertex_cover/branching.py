28384da09b87820f24787716a5ce412db8da5d9c
Adam Cox <ajc785@student.bham.ac.uk>
2020-01-17 15:42:47 +0000

    Draft versions of iterative branching

--- src/vertex_cover/branching.py
+++ src/vertex_cover/branching.py
@@ -3,9 +3,10 @@
 1-Pass BST usingO(k^2·logn)bits
 """
 from networkx import Graph
+from collections import deque
 
 
-def vertex_cover_branching_recursive(graph: Graph, k: int, vc: set = set()) -> set:
+def vertex_cover_branching_dfs_recursive(graph: Graph, k: int, vc: set = set()) -> set:
     """
     Finds a vertex cover of at most size k using branching
 
@@ -45,3 +46,91 @@ def vertex_cover_branching_recursive(graph: Graph, k: int, vc: set = set()) -> s
     vc_right.add(v)
     vc_right = vertex_cover_branching(graph_right, k - 1, vc_right)
     return vc_right
+
+
+def vertex_cover_branching_dfs_iterative(graph: Graph, k: int) -> set:
+    """
+    Finds a vertex cover of at most size k using branching
+
+    Uses an iterative depth-first method
+
+    Parameters
+    ----------
+        graph : Graph
+            The graph to find a vertex cover of
+        k : int
+            Size k
+
+    Returns
+    -------
+        set
+    """
+    stack = []
+    stack.append((graph, []))
+
+    while len(stack) > 0:
+        (graph, vc) = stack.pop()
+
+        if graph.number_of_edges() == 0:
+            return vc
+
+        if len(vc) == k:
+            continue
+
+        (u, v) = list(graph.edges)[0]
+
+        graph_left = graph.copy()
+        graph_left.remove_node(u)
+        vc_left = vc.copy()
+        vc_left.add(u)
+        stack.append((graph_left, vc_left))
+
+        graph_right = graph.copy()
+        graph_right.remove_node(u)
+        vc_right = vc.copy()
+        vc_right.add(u)
+        stack.append((graph_right, vc_right))
+
+
+def vertex_cover_branching_bfs(graph: Graph, k: int) -> set:
+    """
+    Finds a vertex cover of at most size k using branching
+
+    Uses an iterative breadth-first method
+
+    Parameters
+    ----------
+        graph : Graph
+            The graph to find a vertex cover of
+        k : int
+            Size k
+
+    Returns
+    -------
+        set
+    """
+    queue = deque()
+    queue.append((graph, set()))
+
+    while len(queue) > 0:
+        (graph, vc) = queue.popleft()
+
+        if graph.number_of_edges() == 0:
+            return vc
+
+        if len(vc) == k:
+            continue
+
+        (u, v) = list(graph.edges)[0]
+
+        graph_left = graph.copy()
+        graph_left.remove_node(u)
+        vc_left = vc.copy()
+        vc_left.add(u)
+        queue.append((graph_left, vc_left))
+
+        graph_right = graph.copy()
+        graph_right.remove_node(u)
+        vc_right = vc.copy()
+        vc_right.add(u)
+        queue.append((graph_right, vc_right))
