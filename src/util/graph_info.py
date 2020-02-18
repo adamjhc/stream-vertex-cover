513498925e6f62a3a4aae604228795935640ac72
Adam Cox <ajc785@student.bham.ac.uk>
2020-02-18 18:48:27 +0000

    Fixed triangle and closed triangle calculation

--- src/util/graph_info.py
+++ src/util/graph_info.py
@@ -8,8 +8,8 @@ class GraphInfo:
         self.density = nx.density(graph)
         self.transitivity = nx.transitivity(graph)
         self.clustering_coefficient = nx.average_clustering(graph)
-        self.triangles = nx.triangles(graph)
-        self.fraction_closed_triangles = 0
+        self.triangles = sum(nx.triangles(graph).values()) / 3
+        self.fraction_closed_triangles = self.triangles / self._count_paths(graph, 2)
 
     def to_csv(
         self,
@@ -30,13 +30,31 @@ class GraphInfo:
                 str(round(self.density, 3)),
                 str(round(self.transitivity, 3)),
                 str(round(self.clustering_coefficient, 4)),
-                str(len(self.triangles)),
+                str(self.triangles),
                 str(round(self.fraction_closed_triangles, 4)),
                 category,
                 source,
                 link,
             ]
         )
+
+    def _count_paths(self, graph: nx.Graph, length: int):
+        all_paths = []
+        for node in graph:
+            all_paths.extend(self._find_paths(graph, node, length))
+
+        return len(all_paths)
+
+    def _find_paths(self, graph: nx.Graph, u, length: int):
+        if length == 0:
+            return [[u]]
+
+        return [
+            [u] + path
+            for neighbor in graph.neighbors(u)
+            for path in self._find_paths(graph, neighbor, length - 1)
+            if u not in path
+        ]
 
 
 if __name__ == "__main__":
