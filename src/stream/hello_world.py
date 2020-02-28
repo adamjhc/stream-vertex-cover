a4d993da2e16cc85a3699de8d4a808b63f5ea334
Adam Cox <ajc785@student.bham.ac.uk>
2020-02-28 17:57:57 +0000

    Created web view

--- src/stream/hello_world.py
+++ src/stream/hello_world.py
@@ -9,3 +9,12 @@ greetings_topic = app.topic("greetings")
 async def greet(greetings):
     async for greeting in greetings:
         print(greeting)
+
+
+@app.page("/")
+async def get_index(self, request):
+    return self.html("<h1>Faust homepage</h1>")
+
+
+if __name__ == "__main__":
+    app.main()
