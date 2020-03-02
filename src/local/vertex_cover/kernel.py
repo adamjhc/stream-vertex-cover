4a16867c163bd390549a59af693fc7a8cab387be
Adam Cox <ajc785@student.bham.ac.uk>
2020-03-02 17:13:24 +0000

    Static type checking

--- src/local/vertex_cover/kernel.py
+++ src/local/vertex_cover/kernel.py
@@ -2,41 +2,42 @@
 Kernelization Algorithm for Vertex Cover
 (from https://en.wikipedia.org/wiki/Kernelization#Example:_vertex_cover)
 
-1.  If k > 0 and v is a vertex of degree > k, remove v from the graph and 
-    decrease k by 1. Every vertex of size k must contain v since otherwise too 
-    many of its neighbours would have to be picked to cover the incident edges. 
-    Thus, an optimal vertex cover for the original graph may be formed from a 
+1.  If k > 0 and v is a vertex of degree > k, remove v from the graph and
+    decrease k by 1. Every vertex of size k must contain v since otherwise too
+    many of its neighbours would have to be picked to cover the incident edges.
+    Thus, an optimal vertex cover for the original graph may be formed from a
     cover of the reduced problem by adding v back to the cover
-2.  If v is an isolated vertex, remove it. An isolated vertex cannot cover any 
+2.  If v is an isolated vertex, remove it. An isolated vertex cannot cover any
     edges, so in this case v cannot be part of any minimal cover
 3.  If more than k^2 edges remain in the graph, and neither of the previous two
-    rules can be applied, then the graph cannot contain a vertex cover of size 
+    rules can be applied, then the graph cannot contain a vertex cover of size
     k. For, after eliminating all vertices of degree > k, each remaining vertex
-    can only cover at most k edges and a set of k vertices could only cover at 
-    most k^2 edges. In this case, the instance may be replaced by an instance 
+    can only cover at most k edges and a set of k vertices could only cover at
+    most k^2 edges. In this case, the instance may be replaced by an instance
     with two vertices, one edge, and k=0, which also has no solution.
 
-An algorithm that applies these rules repeatedly until no more reductions can 
-be made necessarily terminates with a kernel that has at most k^2 edges and 
-(because each edge has at most two endpoints and there are no isolated 
-vertices) at most 2k^{2} vertices. This kernelization may be implemented in 
+An algorithm that applies these rules repeatedly until no more reductions can
+be made necessarily terminates with a kernel that has at most k^2 edges and
+(because each edge has at most two endpoints and there are no isolated
+vertices) at most 2k^{2} vertices. This kernelization may be implemented in
 linear time. Once the kernel has been constructed, the vertex cover problem may
-be solved by a brute force search algorithm that tests whether each subset of 
-the kernel is a cover of the kernel. Thus, the vertex cover problem can be 
-solved in time O(2^{{2k^{2}}}+n+m) for a graph with n vertices and m edges, 
-allowing it to be solved efficiently when k is small even if n and m are both 
+be solved by a brute force search algorithm that tests whether each subset of
+the kernel is a cover of the kernel. Thus, the vertex cover problem can be
+solved in time O(2^{{2k^{2}}}+n+m) for a graph with n vertices and m edges,
+allowing it to be solved efficiently when k is small even if n and m are both
 large.
 """
+from typing import Optional
 from networkx import Graph
 from .branching import vertex_cover_branching, vertex_cover_branching_stream
 from typing import Tuple
 
 
-def vertex_cover_kernelization(graph: Graph, k: int) -> set:
+def vertex_cover_kernelization(graph: Graph, k: int) -> Optional[set]:
     """
     Finds a vertex cover of size k using kernelization
 
-    Returning a set of at most k vertices that includes the endpoint of every 
+    Returning a set of at most k vertices that includes the endpoint of every
     edge in the graph or None if no such set exists
 
     Parameters
@@ -48,7 +49,8 @@ def vertex_cover_kernelization(graph: Graph, k: int) -> set:
 
     Returns
     -------
-        set
+        Optional[set]
+            Vertex cover if one exists else None
     """
     kernel, vertex_cover = _kernelize(graph, k)
 
@@ -58,7 +60,7 @@ def vertex_cover_kernelization(graph: Graph, k: int) -> set:
     return vertex_cover_branching(kernel, k - len(vertex_cover), vertex_cover)
 
 
-def vertex_cover_kernelization_stream(graph: Graph, k: int) -> set:
+def vertex_cover_kernelization_stream(graph: Graph, k: int) -> Optional[set]:
     """
     Finds a vertex cover of at most size k using kernelization and branching
 
@@ -75,17 +77,17 @@ def vertex_cover_kernelization_stream(graph: Graph, k: int) -> set:
 
     Notes
     -----
-    The kernelization algorithm works by maintaining a maximal matching M. For 
-    every matched vertex v, we also store up to k edges incident on v in a set 
+    The kernelization algorithm works by maintaining a maximal matching M. For
+    every matched vertex v, we also store up to k edges incident on v in a set
     E_m if at the ith update we observe that len(M) > k we report that the size of
-    any vertex cover is more than k and quit. At the end of the stream we run 
+    any vertex cover is more than k and quit. At the end of the stream we run
     the branching algorithm
     """
     edges = list(graph.edges)
 
     kernel = Graph()
     no_in_matching = 0
-    maximal_matching = set()
+    maximal_matching: set = set()
 
     for u, v in edges:
 
@@ -112,7 +114,7 @@ def vertex_cover_kernelization_stream(graph: Graph, k: int) -> set:
 
 def _kernelize(graph: Graph, k: int) -> Tuple[Graph, set]:
     """
-    Kernelizes a graph given size k 
+    Kernelizes a graph given size k
 
     Parameters
     ----------
@@ -123,24 +125,25 @@ def _kernelize(graph: Graph, k: int) -> Tuple[Graph, set]:
 
     Returns
     -------
-        Graph
+        Tuple[Graph, set]
+            Pair of the kernel and vertex cover
     """
     kernel = graph.copy()
     vertex_cover = set()
-    reductionsCanBeMade = True
-    while reductionsCanBeMade:
-        reductionMade = False
+    reductions_can_be_made = True
+    while reductions_can_be_made:
+        reduction_made = False
         for node in list(kernel.nodes):
             degree = kernel.degree[node]
             if k > 0 and degree > k:
-                reductionMade = True
+                reduction_made = True
                 kernel.remove_node(node)
                 vertex_cover.add(node)
                 k -= 1
             elif degree == 0:
                 kernel.remove_node(node)
 
-        if not reductionMade:
-            reductionsCanBeMade = False
+        if not reduction_made:
+            reductions_can_be_made = False
 
     return kernel, vertex_cover
