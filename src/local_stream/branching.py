b3716b3fe2e6d7c52da7234c2d9d5df45e0b4e00
Adam Cox <ajc785@student.bham.ac.uk>
2020-03-02 18:22:06 +0000

    Static type checking

--- src/local_stream/branching.py
+++ src/local_stream/branching.py
@@ -1,5 +1,5 @@
 from io import TextIOWrapper
-from typing import Any, Tuple
+from typing import Optional, Tuple, Iterator
 
 
 class Branching:
@@ -11,14 +11,14 @@ class Branching:
         self.edge_pos = 0
         self.edge_current = None
 
-    def calculate_vc(self, stream: TextIOWrapper) -> set:
+    def calculate_vc(self, stream: TextIOWrapper) -> Optional[set]:
         for bin_string in self._get_binary_strings():
             # Return stream to start
             stream.seek(0)
             # Ignore number of nodes and edges
             stream.readline()
 
-            vertex_cover = set()
+            vertex_cover: set = set()
             bin_string_pos = 0
             self.edge_pos = 0
             self.edge_pos_prev = -1
@@ -42,21 +42,21 @@ class Branching:
 
         return None
 
-    def _get_binary_strings(self) -> list:
+    def _get_binary_strings(self) -> Iterator[str]:
         """
         Generates binary strings up to a given length k
 
         Yields
         ------
-            list
-                List of binary strings
+            str
+                Incrementing binary strings
         """
         for i in range(2 ** self.k):
             yield bin(i)[2:].rjust(self.k, "0")
 
-    def _get_edge(self, stream: TextIOWrapper) -> Tuple[Any, Any]:
+    def _get_edge(self, stream: TextIOWrapper) -> Tuple:
         """
-        
+
         """
         if self.edge_pos != self.edge_pos_prev:
             self.edge_current = stream.readline().split()[:2]
