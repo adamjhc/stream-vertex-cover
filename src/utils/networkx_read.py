2d754f62a33504a0f1562194b96b89404f7dce19
Adam Cox <ajc785@student.bham.ac.uk>
2020-03-02 19:39:05 +0000

    Removed unncessary bits

--- src/utils/networkx_read.py
+++ src/utils/networkx_read.py
@@ -7,24 +7,22 @@ def read_dimacs(path: str) -> Graph:
     with open(path, "r") as file:
         graph = nx.Graph()
 
-        problem_line_seen = False
-
         for line in file.readline():
             line_type = line[0]
 
-            if line_type == "c":
+            if line_type == "e" or line_type == "a":
+                attributes = line.split()
+                u = int(attributes[1])
+                v = int(attributes[2])
+
+                w: int
+                if len(attributes) > 3:
+                    w = int(attributes[3])
+
+                graph.add_edge(u, v, weight=w)
+            elif line_type == "c":
                 continue
             elif line_type == "p":
-                problem_line_seen = True
-            elif line_type == "e" or line_type == "a":
-                if not problem_line_seen:
-                    raise Exception(
-                        "Bad DIMACS format: edge descriptor seen before problem descriptor"
-                    )
-
-                attributes = line.split()
-                u = attributes[1]
-                v = attributes[2]
-                graph.add_edge(u, v)
+                continue
 
     return graph
